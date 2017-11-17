263185021a68f9bfdcb298357e2b71b6f3b2ce29
Salvadorleon <31919685+Salvadorleon@users.noreply.github.com>
2017-11-17 12:27:46 +0100

    Sesion 5 finalizada

--- Practica2/cliente46.c
+++ Practica2/cliente46.c
@@ -27,35 +27,37 @@ int main(int *argc, char *argv[])
 	struct sockaddr_in server_in4;
 	struct sockaddr_in6 server_in6;
 	int address_size = sizeof(server_in4);
-	char buffer_in[1024], buffer_out[1024],input[1024];
-	int recibidos=0,enviados=0;
-	int estado=S_HELO;
+	char buffer_in[1024], buffer_out[1024], input[1024];
+	int recibidos = 0, enviados = 0;
+	int estado = S_HELO;
 	char option;
-	int ipversion=AF_INET;//IPv4 por defecto
+	int ipversion = AF_INET;//IPv4 por defecto
 	char ipdest[256];
-	char default_ip4[16]="127.0.0.1";
-	char default_ip6[64]="::1";
+	char default_ip4[16] = "127.0.0.1";
+	char default_ip6[64] = "::1";
+	char mail_from[40] = ""; //direccion de correo remitente
+	char mail_to[40] = ""; //direccion de correo destino
 
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	int err;
-   
+
 	//Inicialización Windows sockets - SOLO WINDOWS
-	wVersionRequested=MAKEWORD(1,1);
-	err=WSAStartup(wVersionRequested,&wsaData);
-	if(err!=0)
+	wVersionRequested = MAKEWORD(1, 1);
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0)
 		return(0);
 
-	if(LOBYTE(wsaData.wVersion)!=1||HIBYTE(wsaData.wVersion)!=1){
+	if (LOBYTE(wsaData.wVersion) != 1 || HIBYTE(wsaData.wVersion) != 1) {
 		WSACleanup();
 		return(0);
 	}
 	//Fin: Inicialización Windows sockets
-	
-	printf("**************\r\nCLIENTE TCP SENCILLO SOBRE IPv4 o IPv6\r\n*************\r\n");
-	
 
-	do{
+	printf("**************\r\nCLIENTE TCP SENCILLO SOBRE IPv4 o IPv6\r\n*************\r\n");
+
+
+	do {
 
 		printf("CLIENTE> ¿Qué versión de IP desea usar? 6 para IPv6, 4 para IPv4 [por defecto] ");
 		gets_s(ipdest, sizeof(ipdest));
@@ -68,48 +70,48 @@ int main(int *argc, char *argv[])
 			ipversion = AF_INET;
 		}
 
-		sockfd=socket(ipversion,SOCK_STREAM,0);
-		if(sockfd==INVALID_SOCKET){
+		sockfd = socket(ipversion, SOCK_STREAM, 0);
+		if (sockfd == INVALID_SOCKET) {
 			printf("CLIENTE> ERROR\r\n");
 			exit(-1);
 		}
-		else{
+		else {
 			printf("CLIENTE> Introduzca la IP destino (pulsar enter para IP por defecto): ");
-			gets_s(ipdest,sizeof(ipdest));
+			gets_s(ipdest, sizeof(ipdest));
 
 			//Dirección por defecto según la familia
-			if(strcmp(ipdest,"")==0 && ipversion==AF_INET)
-				strcpy_s(ipdest,sizeof(ipdest),default_ip4);
+			if (strcmp(ipdest, "") == 0 && ipversion == AF_INET)
+				strcpy_s(ipdest, sizeof(ipdest), default_ip4);
 
-			if(strcmp(ipdest,"")==0 && ipversion==AF_INET6)
-				strcpy_s(ipdest, sizeof(ipdest),default_ip6);
+			if (strcmp(ipdest, "") == 0 && ipversion == AF_INET6)
+				strcpy_s(ipdest, sizeof(ipdest), default_ip6);
 
-			if(ipversion==AF_INET){
-				server_in4.sin_family=AF_INET;
-				server_in4.sin_port=htons(TCP_SERVICE_PORT);
+			if (ipversion == AF_INET) {
+				server_in4.sin_family = AF_INET;
+				server_in4.sin_port = htons(TCP_SERVICE_PORT);
 				//server_in4.sin_addr.s_addr=inet_addr(ipdest);
-				inet_pton(ipversion,ipdest,&server_in4.sin_addr.s_addr);
-				server_in=(struct sockaddr*)&server_in4;
+				inet_pton(ipversion, ipdest, &server_in4.sin_addr.s_addr);
+				server_in = (struct sockaddr*)&server_in4;
 				address_size = sizeof(server_in4);
 			}
 
-			if(ipversion==AF_INET6){
+			if (ipversion == AF_INET6) {
 				memset(&server_in6, 0, sizeof(server_in6));
-				server_in6.sin6_family=AF_INET6;
-				server_in6.sin6_port=htons(TCP_SERVICE_PORT);
-				inet_pton(ipversion,ipdest,&server_in6.sin6_addr);
-				server_in=(struct sockaddr*)&server_in6;
+				server_in6.sin6_family = AF_INET6;
+				server_in6.sin6_port = htons(TCP_SERVICE_PORT);
+				inet_pton(ipversion, ipdest, &server_in6.sin6_addr);
+				server_in = (struct sockaddr*)&server_in6;
 				address_size = sizeof(server_in6);
 			}
 
-			estado=S_HELO;
+			estado = S_HELO;
 
-			if(connect(sockfd, server_in, address_size)==0){
-				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
-			
+			if (connect(sockfd, server_in, address_size) == 0) {
+				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n", ipdest, TCP_SERVICE_PORT);
+
 				//Inicio de la máquina de estados
-				do{
-					switch(estado){
+				do {
+					switch (estado) {
 					case S_HELO:
 						// Recibimos el mensaje de bienvenida
 						sprintf_s(buffer_out, sizeof(buffer_out), "HELO hola\r\n");
@@ -117,82 +119,103 @@ int main(int *argc, char *argv[])
 
 						//METER AQUI EL SUBJECT ANTES DE HACER EL ENVIO DEL CORREO
 
-					case S_DATA:
+					case S_MAIL_FROM:
 						// establece la conexion de aplicacion 
-						printf("CLIENTE> Introduzca su host (enter para salir): ");
-						gets_s(input,sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s%s%s",HELO,CRLF);
-							estado=S_QUIT;
+						printf("CLIENTE> Introduce tu direccion de correo: ");
+						gets(input);
+						strcpy(mail_from, input);
+						if (strcmp(input, "QUIT") == 0)
+						{
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", QUIT, CRLF);
 						}
 						else
+						{
+							sprintf_s(buffer_out, sizeof(buffer_out), "MAIL FROM: %s%s", input, CRLF);
+						}
+						break;
 
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",HELO,input,CRLF);
-						break;
-					case S_PASS:
-						printf("CLIENTE> Introduzca la clave (enter para salir): ");
-						gets_s(input,sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",QUIT,CRLF);
-							estado=S_QUIT;
+					case S_RCPT_TO:
+						printf("CLIENTE> Intrduce el correo del destinatario: ");
+						gets(input);
+						strcpy(mail_to, input);
+						if (strcmp(input, "QUIT") == 0)
+						{
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", QUIT, CRLF);
 						}
 						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",PW,input,CRLF);
+						{
+							sprintf_s(buffer_out, sizeof(buffer_out), "RCPT TO: %s%s", input, CRLF);
+						}
 						break;
-					case S_DATA:
+					case S_SEND:
 						printf("CLIENTE> Introduzca datos (enter o QUIT para salir): ");
 						gets_s(input, sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",QUIT,CRLF);
-							estado=S_QUIT;
+						if (strlen(input) == 0) {
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", QUIT, CRLF);
+							estado = S_QUIT;
 						}
 						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",ECHO,input,CRLF);
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s %s%s", OKDATA, input, CRLF);
 						break;
-				
-					}
 
-					if(estado!=S_HELO){
-						enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
-						if(enviados==SOCKET_ERROR){
-							 estado=S_QUIT;
-							 continue;
-						}
-					}
-						
-					recibidos=recv(sockfd,buffer_in,512,0);
-					if(recibidos<=0){
-						DWORD error=GetLastError();
-						if(recibidos<0){
-							printf("CLIENTE> Error %d en la recepción de datos\r\n",error);
-							estado=S_QUIT;
-						}
-						else{
-							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
-							estado=S_QUIT;
-						}
-					}else{
-						buffer_in[recibidos]=0x00;
-						printf(buffer_in);
-						if(estado!=S_DATA && strncmp(buffer_in,OK,2)==0) 
-							estado++;  
-					}
 
-				}while(estado!=S_QUIT);		
+
+					case S_DATA_MAIL:
+						break;
+
+					case S_QUIT:
+						sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", QUIT, CRLF);
+						estado = S_QUIT;
+						break;
+
+
+
+						if (estado != S_HELO) {
+							enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
+							if (enviados == SOCKET_ERROR) {
+								estado = S_QUIT;
+								continue;
+							}
+						}
+
+						recibidos = recv(sockfd, buffer_in, 512, 0);
+						if (recibidos <= 0) {
+							DWORD error = GetLastError();
+							if (recibidos < 0) {
+								printf("CLIENTE> Error %d en la recepción de datos\r\n", error);
+								estado = S_QUIT;
+							}
+							else {
+								printf("CLIENTE> Conexión con el servidor cerrada\r\n");
+								estado = S_QUIT;
+							}
+						}
+						else {
+							buffer_in[recibidos] = 0x00;
+							printf(buffer_in);
+							if (estado != S_SEND && strncmp(buffer_in, OK, 2) == 0)
+								estado++;
+						}
+
+					}while (estado != S_QUIT);
+				}
+			
+
+/*		else {
+				int error_code = GetLastError();
+				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n", ipdest, TCP_SERVICE_PORT);
 			}
-			else{
-				int error_code=GetLastError();
-				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
-			}		
 			// fin de la conexion de transporte
 			closesocket(sockfd);
-			
-		}	
-		printf("-----------------------\r\n\r\nCLIENTE> Volver a conectar (S/N)\r\n");
-		option=_getche();
 
-	}while(option!='n' && option!='N');
 
-	WSACleanup();
-	return(0);
-}
+			printf("-----------------------\r\n\r\nCLIENTE> Volver a conectar (S/N)\r\n");
+			option = _getche();
+
+				} while (option != 'n' && option != 'N');
+
+				WSACleanup();
+				return(0);
+			}
+		}
+	*/
--- Practica2/protocol.h
+++ Practica2/protocol.h
@@ -17,13 +17,12 @@
 //ESTADOS
 #define S_CONNECT 0
 #define S_HELO 1
-#define S_DATA 2
-#define S_MAIL_FROM 3
-#define S_RCPT_TO 4
-#define S_SEND 5
-#define S_DATA_MAIL 6
-#define S_QUIT 7
-#define S_EXIT 8
+#define S_MAIL_FROM 2
+#define S_RCPT_TO 3
+#define S_SEND 4
+#define S_DATA_MAIL 5
+#define S_QUIT 6
+#define S_EXIT 7
 
 //PUERTO DEL SERVICIO
 #define TCP_SERVICE_PORT	25
